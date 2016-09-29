1be58cc62d0f1f69a597ea0596c34ef78037964d
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2016-09-29 11:24:36 +0200

    nRF52 vector relocation IAR

--- hal/targets/cmsis/TARGET_NORDIC/TARGET_NRF5/dispatch_IRQn_handler.c
+++ hal/targets/cmsis/TARGET_NORDIC/TARGET_NRF5/dispatch_IRQn_handler.c
@@ -43,11 +43,15 @@
 #include "section_vars.h"
 
 #if defined(__CC_ARM)
-__attribute__ ((section("noinit")))
-#elif defined(__CC_ARM)
-__attribute__ ((section(".noinit")))
+    __attribute__ ((section("noinit"),zero_init))
+    uint32_t nrf_dispatch_vector[NVIC_NUM_VECTORS];
+#elif defined(__GNUC__)
+    __attribute__ ((section(".noinit")))
+    uint32_t nrf_dispatch_vector[NVIC_NUM_VECTORS];
+#elif defined(__ICCARM__)
+    uint32_t nrf_dispatch_vector[NVIC_NUM_VECTORS] @ ".noinit";
 #endif
-uint32_t nrf_dispatch_vector[NVIC_NUM_VECTORS];
+
 
 typedef void (*generic_irq_handler_t)(void);
 
